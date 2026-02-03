497b296bedfa68da18f856d624f12e47c6020e4c
Jertta Kivistö <jertta@gmail.com>
2026-02-03 21:56:02 +0200

    Added some logic to volume sliders
    still very wip

--- Assets/Scripts/UI/SettingsMenuController.cs
+++ Assets/Scripts/UI/SettingsMenuController.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class SettingsMenuController : MonoBehaviour
 {
-    
+    public AudioMixer audioMixer;
+
+    public void SetMusicVolume(float volume)
+    {
+        Debug.Log("Volume set to: " + volume);
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        Debug.Log("Volume set to: " + volume);
+        audioMixer.SetFloat("sfxVolume", volume);
+    }
+
+    public void SetVoiceVolume(float volume)
+    {
+        Debug.Log("Volume set to: " + volume);
+        audioMixer.SetFloat("voiceVolume", volume);
+    }
 }
